f13459e4bfe3ca618c54fb635ac128a4e0b7bebd
Sander de Ruiter <sdr@16april1973.nl>
2026-01-17 21:50:06 +0100

    All

---
+++ test/bases/ccdexplorer/ms_events_and_impacts/test_ms_events_and_impacts.py
@@ -0,0 +1,30 @@
+# from ccdexplorer.tooter.core import Tooter
+# from pymongo import ReplaceOne
+# import pytest
+# from unittest.mock import patch, MagicMock
+
+# from ccdexplorer.mongodb import MongoDB, MongoMotor, Collections
+# from ccdexplorer.grpc_client import GRPCClient
+# from ccdexplorer.domain.generic import NET
+# from ccdexplorer.ms_events_and_impacts.subscriber import Subscriber
+
+
+# @pytest.mark.asyncio
+# async def test_sponsored_tx(
+#     grpcclient: GRPCClient, tooter: Tooter, motormongo: MongoMotor, mongodb: MongoDB
+# ):
+#     mock_bulk_write = MagicMock()
+#     mock_collection = MagicMock()
+#     mock_collection.bulk_write = mock_bulk_write
+#     with (
+#         patch.object(tooter, "send_to_tooter") as _,
+#         patch.dict(
+#             mongodb.mainnet,
+#             {Collections.all_account_addresses: mock_collection},
+#             clear=False,
+#         ),
+#     ):
+#         subscriber = Subscriber(grpcclient, tooter, motormongo, mongodb)
+#         impacted_addresses = await subscriber.extract_impacted_addresses_from_tx(tx)
+#         mock_bulk_write.assert_called_once()
+#
